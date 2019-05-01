a8561ce7152efe2f52f1a17d823a048a6e2e24ca
Michael Bock <me@thought2.de>
2019-05-01 07:34:22 +0200

    Load and draw multiple images at random positions.

--- 2019-04-29/lorem-picsum/sketch.js
+++ 2019-04-29/lorem-picsum/sketch.js
@@ -1,15 +1,42 @@
+const config = {
+  countImgs: 30
+};
+
 new p5(function(p) {
-  let img;
+  let imgs = [];
+
+  for (let i = 0; i < config.countImgs; i++) {
+    imgs[i] = { position: { x: p.random(), y: p.random() } };
+  }
+
   p.preload = () => {
-    img = p.loadImage("https://picsum.photos/id/36/200/300.jpg");
+    const url = `https://picsum.photos/v2/list?page=2&limit=${
+      config.countImgs
+    }`;
+    p.httpGet(url, result => {
+      const items = JSON.parse(result);
+
+      items.forEach((item, i) => {
+        imgs[i].imgData = p.loadImage(item.download_url);
+      });
+    });
   };
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.image(img, 0, 0);
   };
 
   p.draw = () => {
     //p.background(200);
+    imgs.forEach(img => {
+      if (!img.imgData) return;
+      p.image(
+        img.imgData,
+        img.position.x * p.width,
+        img.position.y * p.height,
+        20,
+        20
+      );
+    });
   };
 });
