3ce182b77f6c8e1f422efc48f4ceb9d6383fb405
Michael Bock <me@thought2.de>
2019-05-01 07:10:46 +0200

    Load one image from the API.

--- 2019-04-29/lorem-picsum/sketch.js
+++ 2019-04-29/lorem-picsum/sketch.js
@@ -1,9 +1,15 @@
 new p5(function(p) {
+  let img;
+  p.preload = () => {
+    img = p.loadImage("https://picsum.photos/id/36/200/300.jpg");
+  };
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.image(img, 0, 0);
   };
 
   p.draw = () => {
-    p.background(200);
+    //p.background(200);
   };
 });
